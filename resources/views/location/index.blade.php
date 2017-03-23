5a77df75dca10f8e8be89d336cf20e2f26ef9dfe
fadhil imran <fdhlmrn@fdhlmrn.local>
2017-03-23 20:43:53 +0800

    coba1

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -17,31 +17,54 @@
         padding: 0;
       }
     </style>
-  </head>
-  <body>
-    <div id="map"></div>
-    <div class="container">
-      <div class="col-sm-4">
-        <h1>Add Vendor, Location</h1>
-        <form action="url (/vendor/add)">
-          <div class="form-group">
-            <label for=""></label>
-            <label type="text" class="form-control input-sm" name=""></label>
-            
-          </div>
-          
-        </form>
-        
-        
-      </div>
-      
-    </div>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js" ></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key= AIzaSyDuYeuRRArPgWsCE_Zrmzd6WcrMI08dNEU&callback=initMap" type="text/javascript" ></script>
+
+<div class="container">
+  <div class="col-sm-4">
+    <h1>Add Vendor, Location</h1>
+    <form class="form-horizontal" action="{{ url('/map') }}">
 
 
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key= AIzaSyDuYeuRRArPgWsCE_Zrmzd6WcrMI08dNEU&callback=initMap" type="text/javascript">
-    </script>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js">
-    </script>
-  </body>
+        <div class="form-group">
+          <label for="">Map</label>
+          <input type="text" id="searchmap">
+          <div id="map-canvas"></div>
+        </div> 
+        <div class="form-group">
+          <label for="">Lat</label>
+          <input type="text" class="form-control input-sm" name="lat" id="lat">
+        </div>
+        <div class="form-group">
+          <label for="">Lng</label>
+          <input type="text" class="form-control input-sm" name="lng" id="lng">
+        </div> 
+
+        <button class="btn btn-sm btn-danger"></button>
+      </form>
+
+  </div>  
+</div>
+
+<script>
+  
+      var map = new google.maps.Marker(document.getElementById('map-canvas'),{
+          center:{
+            lat: 27.72,
+            lng: 85.36
+          },
+          zoom:15
+      });
+
+      var marker = new google.maps.Marker({
+        position: {
+          lat: 27.72,
+          lng: 85.36
+        },
+        map: map
+      });
+
+</script>
+
 </html>
