a3a0419ec4a3ca61f35db65d82bea6d74f96e2b2
fdhlmrn <thepadei@gmail.com>
2017-05-23 22:32:24 +0800

    error index

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -29,7 +29,7 @@
                 <input class="form-control" id="longitude" type="hidden" name="longitude">
             <div class="form-group">
                 <div class="col-sm-offset-9 col-sm-10">
-                  <a href="{{ url('/home') }}" class="btn btn-danger">Cancel</a>
+                  <a href="{{ url('/home') }}" class="btn btn-danger">List All</a>
                   <button type="submit" class="btn btn-success">Save</button>
                 </div>
               </div>
